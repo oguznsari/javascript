4d936d3ad9a823180c64dfd34beb1d10e6ecc6d5
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-09-12 08:19:45 +0300

    build command added to the cli

--- ts-cli/src/index.ts
+++ ts-cli/src/index.ts
@@ -2,6 +2,9 @@
 
 import yargs from "yargs";
 import fs from "fs";
+import { error } from "console";
+import { stderr, stdout } from "process";
+import { exec } from "child_process";
 
 const argv = yargs
   .command({
@@ -13,10 +16,22 @@ const argv = yargs
       createStyleCSS();
     },
   })
+  .command("build", "Run npm run build")
   .help().argv as { _: string[] };
 
 if (argv._.includes("create")) {
   createStyleCSS();
+}
+
+if (argv._.includes("build")) {
+  exec("npm run build", (error: any, stdout: any, stderr: any) => {
+    if (error) {
+      console.error(`Error: ${stderr}`);
+    }
+
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
 }
 
 function createStyleCSS() {
