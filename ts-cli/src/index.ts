fbcaf840a3af2840664519a093d6df2e98f4d618
Oğuzhan SARI <oguzn.sari@gmail.com>
2023-09-12 08:48:14 +0300

    new -> create new project

--- ts-cli/src/index.ts
+++ ts-cli/src/index.ts
@@ -2,9 +2,15 @@
 
 import yargs from "yargs";
 import fs from "fs";
+import { exec } from "child_process";
+import readline from "readline";
 import { error } from "console";
 import { stderr, stdout } from "process";
-import { exec } from "child_process";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const argv = yargs
   .command({
@@ -17,7 +23,18 @@ const argv = yargs
     },
   })
   .command("build", "Run npm run build")
-  .help().argv as { _: string[] };
+  .command("new", "Create a new Project", {
+    name: {
+      describe: "Project name",
+      demandOption: true,
+      type: "string",
+      alias: "n",
+    },
+  })
+  .help().argv as {
+  [key: string]: unknown;
+  _: string[];
+};
 
 if (argv._.includes("create")) {
   createStyleCSS();
@@ -27,11 +44,53 @@ if (argv._.includes("build")) {
   exec("npm run build", (error: any, stdout: any, stderr: any) => {
     if (error) {
       console.error(`Error: ${stderr}`);
+      return;
     }
 
     console.log(`stdout: ${stdout}`);
     console.error(`stderr: ${stderr}`);
   });
+}
+
+if (argv._.includes("new")) {
+  let projectName = argv.name as string;
+
+  if (!projectName) {
+    rl.question("Please enter a project name", (inputName: string) => {
+      projectName = inputName;
+      rl.close();
+      createNewProject(projectName);
+    });
+  } else {
+    createNewProject(projectName);
+  }
+}
+
+function createNewProject(projectName: string) {
+  fs.mkdirSync(projectName);
+
+  exec(
+    `git clone https://github.com/oguznsari/nextjs-ai-platform ${projectName}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${stderr}`);
+        return;
+      }
+
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+
+      exec(`cd ${projectName} && npm install`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${stderr}`);
+          return;
+        }
+
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+    }
+  );
 }
 
 function createStyleCSS() {
